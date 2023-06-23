1666f28a4112a289c60683c6124c692dc26efd5f
Jack Hall <jack@Kims-MacBook-Pro.local>
2023-06-23 18:56:16 -0500

    dev: add treesitter, telescope, keymaps

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -10,3 +10,9 @@ vim.keymap.set('n', '<C-l>', '<C-W>L')
 vim.keymap.set('n', '<C-h>', '<C-W>H')
 vim.keymap.set('n', '<C-j>', '<C-W>J')
 vim.keymap.set('n', '<C-k>', '<C-W>K')
+
+
+-- telescope
+vim.keymap.set('n', '<Leader>f', ':Telescope find_files<cr>')
+vim.keymap.set('n', '<Leader>st', ':Telescope live_grep<cr>')
+vim.keymap.set('n', '<Leader>sk', ':Telescope keymaps<cr>')
--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -12,13 +12,105 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-  {
-    "folke/tokyonight.nvim",
-    lazy = false, -- make sure we load this during startup if it is your main colorscheme
-    priority = 1000, -- make sure to load this before all the other start plugins
-    config = function()
-      -- load the colorscheme here
-      vim.cmd([[colorscheme tokyonight]])
-    end,
-  },
+    {
+        "folke/tokyonight.nvim",
+        lazy = false, -- make sure we load this during startup if it is your main colorscheme
+        priority = 1000, -- make sure to load this before all the other start plugins
+        config = function()
+          -- load the colorscheme here
+          vim.cmd([[colorscheme tokyonight]])
+        end,
+    },
+    {
+      "nvim-treesitter/nvim-treesitter",
+      version = false, -- last release is way too old and doesn't work on Windows
+      build = ":TSUpdate",
+      event = { "BufReadPost", "BufNewFile" },
+      dependencies = {
+        {
+          "nvim-treesitter/nvim-treesitter-textobjects",
+          init = function()
+            -- disable rtp plugin, as we only need its queries for mini.ai
+            -- In case other textobject modules are enabled, we will load them
+            -- once nvim-treesitter is loaded
+            require("lazy.core.loader").disable_rtp_plugin("nvim-treesitter-textobjects")
+            load_textobjects = true
+          end,
+        },
+      },
+      cmd = { "TSUpdateSync" },
+      keys = {
+        { "<c-space>", desc = "Increment selection" },
+        { "<bs>", desc = "Decrement selection", mode = "x" },
+      },
+      ---@type TSConfig
+      opts = {
+        highlight = { enable = true },
+        indent = { enable = true },
+        ensure_installed = {
+          "bash",
+          "c",
+          "go",
+          "html",
+          "javascript",
+          "json",
+          "lua",
+          "luadoc",
+          "luap",
+          "markdown",
+          "markdown_inline",
+          "python",
+          "query",
+          "regex",
+          "tsx",
+          "typescript",
+          "vim",
+          "vimdoc",
+          "yaml",
+        },
+        incremental_selection = {
+          enable = true,
+          keymaps = {
+            init_selection = "<C-space>",
+            node_incremental = "<C-space>",
+            scope_incremental = false,
+            node_decremental = "<bs>",
+          },
+        },
+      },
+      ---@param opts TSConfig
+      config = function(_, opts)
+        if type(opts.ensure_installed) == "table" then
+          ---@type table<string, boolean>
+          local added = {}
+          opts.ensure_installed = vim.tbl_filter(function(lang)
+            if added[lang] then
+              return false
+            end
+            added[lang] = true
+            return true
+          end, opts.ensure_installed)
+        end
+        require("nvim-treesitter.configs").setup(opts)
+
+        if load_textobjects then
+          -- PERF: no need to load the plugin, if we only need its queries for mini.ai
+          if opts.textobjects then
+            for _, mod in ipairs({ "move", "select", "swap", "lsp_interop" }) do
+              if opts.textobjects[mod] and opts.textobjects[mod].enable then
+                local Loader = require("lazy.core.loader")
+                Loader.disabled_rtp_plugins["nvim-treesitter-textobjects"] = nil
+                local plugin = require("lazy.core.config").plugins["nvim-treesitter-textobjects"]
+                require("lazy.core.loader").source_runtime(plugin.dir, "plugin")
+                break
+              end
+            end
+          end
+        end
+      end,
+    },
+    {
+        'nvim-telescope/telescope.nvim', tag = '0.1.1',
+        dependencies = { 'nvim-lua/plenary.nvim' }
+    },
 })
